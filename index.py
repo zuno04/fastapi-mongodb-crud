3020553588a9c55da82ae268a04a0d46928a8408
zuno04 <alaincesar@hotmail.fr>
2021-09-21 13:05:51 +0100

    Server API DONE

--- index.py
+++ index.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+
+client_apps = ["http://localhost:3000"] # React Frontend App
 
 # Create App
 app = FastAPI()
 
 # Register the routers
 app.include_router(student_router)
+
+# Register App with CORS middleware to allow resource sharing betwen different domains
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
